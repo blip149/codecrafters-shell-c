2e17fcfe9f619865ef5a7a34d3264a36b47b1c4c
blip149 <macinpeter15@gmail.com>
2026-05-07 08:47:41 +0300

    codecrafters submit [skip ci]

--- src/main.c
+++ src/main.c
@@ -1,11 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main(int argc, char *argv[]) {
   // Flush after every printf
   setbuf(stdout, NULL);
 
   printf("$ ");
+
+  char command[1024];
+  fgets(command, sizeof(command), stdin);
+  command[strspn(command, "\n")] = '\0';
 
   return 0;
 }
