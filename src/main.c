8ca66beee1663cf50ff9447c47f2547e9706d40e
blip149 <macinpeter15@gmail.com>
2026-05-07 08:50:09 +0300

    codecrafters submit [skip ci]

--- src/main.c
+++ src/main.c
@@ -10,7 +10,9 @@ int main(int argc, char *argv[]) {
 
   char command[1024];
   fgets(command, sizeof(command), stdin);
-  command[strspn(command, "\n")] = '\0';
+  command[strcspn(command, "\n")] = '\0';
+
+  printf("%s: command not found\n", command);
 
   return 0;
 }
