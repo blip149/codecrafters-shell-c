f7143df08aefb88f9cd2dd6c35d84df41970812a
blip149 <macinpeter15@gmail.com>
2026-05-07 09:29:40 +0300

    codecrafters submit [skip ci]

--- src/main.c
+++ src/main.c
@@ -5,14 +5,20 @@
 int main(int argc, char *argv[]) {
   // Flush after every printf
   setbuf(stdout, NULL);
-
-  printf("$ ");
-
   char command[1024];
-  fgets(command, sizeof(command), stdin);
-  command[strcspn(command, "\n")] = '\0';
 
-  printf("%s: command not found\n", command);
+  while(1) {
+    printf("$ ");
+
+    if (fgets(command, sizeof(command), stdin) == NULL){
+      printf("\nExiting..\n");
+      break;
+    }
+
+    int spn = strcspn(command, "\n");
+    command[spn] = '\0';
+  }
 
   return 0;
 }
+
