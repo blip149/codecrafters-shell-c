239fd84cc8e2203424455a97737258b7bc56b02a
blip149 <macinpeter15@gmail.com>
2026-05-27 12:33:15 +0300

    codecrafters submit [skip ci]

--- src/command.c
+++ src/command.c
@@ -99,7 +99,7 @@ int handle_command(Command* cmd) {
         return 0;
     }else if (strcmp(cmd->cmd, "echo")==0){
         for (int i = 0; i<cmd->argc; i++){
-            printf("%s%s",  (i == cmd->argc - 1) ? cmd->args[i]:" ");
+            printf("%s%s", cmd->args[i], (i == cmd->argc - 1) ? "":" ");
         }
         printf("\n");
         
